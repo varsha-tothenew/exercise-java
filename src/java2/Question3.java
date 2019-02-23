3a6c994d8ec105cfa90b6154b001ba03882397ad
varsha <varsha.deora@tothenew.com>
2019-02-23 18:40:45 +0530

    Question 3 commited

--- src/java2/Question3.java
+++ src/java2/Question3.java
@@ -1,4 +1,25 @@
 package java2;
 
+// 3. WAP to produce NoClassDefFoundError and ClassNotFoundException exception.
 public class Question3 {
+    public static void main(String[] args)
+
+    {
+
+        try
+
+        {
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        }catch (ClassNotFoundException e)
+
+        {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
 }
